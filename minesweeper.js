544f08d93e39b2642582a27c1dcdc9a2acc28bd6
tmehta2442 <tmehta@post.harvard.edu>
2017-11-20 18:13:14 -0500

    just testing

---
+++ minesweeper.js
@@ -0,0 +1,19 @@
+//just messing around
+
+const exampleBoard = [
+  [' ', ' ', ' '],
+  [' ', ' ', ' '],
+  [' ', ' ', ' ']
+];
+
+const printBoard = board => {
+  console.log('Current Board: ');
+  console.log(board[0].join(' | '));
+  console.log(board[1].join(' | '));
+  console.log(board[2].join(' | '));
+};
+
+printBoard(exampleBoard);
+exampleBoard[0][1] = '1';
+exampleBoard[1][0] = 'B';
+printBoard(exampleBoard);
